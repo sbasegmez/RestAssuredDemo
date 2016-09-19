5fc7b8379507bbd1d66f2ff3ac3d6f36844db4b1
Serdar Basegmez <serdar.basegmez@developi.com>
2016-09-19 12:26:07 +0300

    Add authentication and modify response type for /test

--- com.developi.wink.demo/src/com/developi/wink/demo/api/TestResource.java
+++ com.developi.wink.demo/src/com/developi/wink/demo/api/TestResource.java
@@ -4,8 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-import com.ibm.commons.util.io.json.JsonJavaObject;
 import com.ibm.domino.osgi.core.context.ContextInfo;
 
 import lotus.domino.NotesException;
@@ -16,11 +16,18 @@ public class TestResource {
 
 	@GET
 	public Response test(){
-		JsonJavaObject resp = new JsonJavaObject();
-		
 		try {
-			resp.put("message", "Hello " + getUserSession().getEffectiveUserName());
-			return Response.ok().type(MediaType.APPLICATION_JSON).entity(resp.toString()).build();
+			String userName = getUserSession().getEffectiveUserName();
+			
+			if("Anonymous".equals(userName)) {
+				// If connected through a browser, this response will signal for authentication and redirected to the login form. 
+				// However, be careful with 40x responses. If session authentication is enabled for the server,
+				// The consumer might receive the login form and 200-OK response. This would be a code-breaker.
+				return Response.status(Status.UNAUTHORIZED).build();
+			} else {
+				String message = "<b>Hello " + userName + "</b>";
+				return Response.ok().type(MediaType.TEXT_HTML).entity(message).build();
+			}
 		} catch (NotesException e) {
 			e.printStackTrace();
 			return Response.serverError().build();
