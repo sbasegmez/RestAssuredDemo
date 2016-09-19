d02306009898aadd61e5cca37bd19060a3e302ff
Serdar Basegmez <serdar.basegmez@developi.com>
2016-09-19 14:54:34 +0300

    Added comments for CustomWinkServlet

--- com.developi.wink.template/src/com/developi/wink/template/servlet/CustomWinkServlet.java
+++ com.developi.wink.template/src/com/developi/wink/template/servlet/CustomWinkServlet.java
@@ -15,20 +15,41 @@ public class CustomWinkServlet extends AbstractRestServlet {
 	
 	@Override
 	protected void doDestroy() {
-		System.out.println("Custom Wink Servlet destroyed...");
+		
+		// This part will run on HTTP quit.
+		// You can inject your code if you want to execute anything special before the project unloaded.
+		System.out.println("Wink Servlet destroyed...");
+		
 		super.doDestroy();
 	}
 
 	@Override
 	protected void doInit() throws ServletException {
 		super.doInit();
-		System.out.println("Custom Wink Servlet initialized.");
+
+		// This part will run once on the first request taken by the servlet.
+		// You can inject your code if you want to execute anything special on start.
+		System.out.println("Wink Servlet initialized.");
+		
 	}
 
 	@Override
 	protected void doService(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
+			
+			// This part will run every time a request taken. You can inject your code here if you need to do anything
+			// before the request fulfilled.
+			//
+			// For instance, you might want to check a specific authentication key here, force SSL connection
+			// or log the request in case of debugging, etc.
+			
 			super.doService(request, response);
+			
+			// This part will run when response is ready. You can inject your code here if you need to do anything
+			// after the request fulfilled.
+			//
+			// For instance, you might want to add a specific response tag here or log the response for debugging.
+			
 		} catch(Throwable t) {
 			t.printStackTrace();
 			response.sendError(500, "Application failed!");
